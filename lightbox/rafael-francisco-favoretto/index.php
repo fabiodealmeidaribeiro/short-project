54b00340924ae840678dffb6d90d11616b9fbf8f
fabiodealmeidaribeiro <far040615@gmail.com>
2023-10-28 14:51:59 -0300

    2023/10/28-14:51

--- lightbox/rafael-francisco-favoretto/index.php
+++ lightbox/rafael-francisco-favoretto/index.php
@@ -6,15 +6,9 @@
     $is_about = property_exists($is_about, 'about') ? $is_about->about : [];
     echo FooterBuilder ([
         [
-            ...IsTrue($is_about)
-            ? (property_exists($is_about, 'title') ? [ '<h1>' . trim($is_about->title) . '</h1>' ] : [])
-            : [],
-            ...IsTrue($is_about)
-            ? (property_exists($is_about, 'subtitle') ? [ '<h2>' . trim($is_about->subtitle) . '</h2>' ] : [])
-            : [],
-            ...IsTrue($is_about)
-            ? (property_exists($is_about, 'description') ? array_map(function($i) { return '<p>' . trim($i) . '</p>'; }, $is_about->description) : [])
-            : [],
+            ...IsTrue($is_about) ? (property_exists($is_about, 'title') ? [ '<h1>' . trim($is_about->title) . '</h1>' ] : []) : [],
+            // ...IsTrue($is_about) ? (property_exists($is_about, 'subtitle') ? [ '<h2>' . trim($is_about->subtitle) . '</h2>' ] : []) : [],
+            ...IsTrue($is_about) ? (property_exists($is_about, 'description') ? array_map(function($i) { return '<p>' . trim($i) . '</p>'; }, $is_about->description) : []) : [],
         ],
         ...property_exists($is_footer, 'footer') ? $is_footer->footer : [],
     ]);
