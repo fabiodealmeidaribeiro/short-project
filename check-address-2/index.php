fbc63b361169ff78d4e0125397a5d38a2c64da7f
fabiodealmeidaribeiro <far040615@gmail.com>
2023-12-05 21:53:06 -0300

    2023/12/05-21:53

--- check-address-2/index.php
+++ check-address-2/index.php
@@ -1,17 +1,17 @@
 <?php
     include_once('./functions.php');
-    echo HeaderDisplay ();
+    echo function_exists ('HeaderDisplay') ? HeaderDisplay () : null;
         echo implode('', [
             '<header', ...IsTrue(AllClasses()['header']) ? [ ' class=\'' . AllClasses()['header'] . '\'' ] : [], '>',
             '</header>',
             '<main', ...IsTrue(AllClasses()['main']) ? [ ' class=\'' . AllClasses()['main'] . '\'' ] : [], '>',
-                FormBuilder([ 'action' => 'process.php', 'database' => 'database.json' ]),
+                function_exists ('FormBuilder') ? FormBuilder([ 'action' => 'process.php', 'database' => 'database.json' ]) : null,
             '</main>',
             '<footer', ...IsTrue(AllClasses()['footer']) ? [ ' class=\'' . AllClasses()['footer'] . '\'' ] : [], '>',
-                LinkedinDisplay (),
+                function_exists ('LinkedinDisplay') ? LinkedinDisplay ([ 'user' => 'fabiodealmeidaribeiro' ]) : null,
             '</footer>',
         ]);
-        echo CallerBuilder ();
-        echo ContainerBuilder ();
-    echo FooterDisplay ();
+        echo function_exists ('CallerBuilder') ? CallerBuilder () : null;
+        echo function_exists ('ContainerBuilder') ? ContainerBuilder () : null;
+    echo function_exists ('FooterDisplay') ? FooterDisplay () : null;
 ?>
